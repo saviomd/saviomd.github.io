4bc200523d56d0d87231f0ee09a04b3309d20feb
Sávio Mendes <saviomd@users.noreply.github.com>
2022-09-18 21:01:22 -0300

    Add ga4; Update dependencies

--- src/utils/trackGaPageView.ts
+++ src/utils/trackGaPageView.ts
@@ -1,8 +1,11 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const trackGaPageView = () => {
-  ReactGA.initialize(import.meta.env.PROD ? "UA-22412489-1" : "UA-22412489-2");
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  ReactGA.initialize([
+    { trackingId: import.meta.env.PROD ? "G-JQYYG4Y06V" : "G-N6KHSH4BNY" },
+    { trackingId: import.meta.env.PROD ? "UA-22412489-1" : "UA-22412489-2" },
+  ]);
+  ReactGA.send("pageview");
 };
 
 export default trackGaPageView;
