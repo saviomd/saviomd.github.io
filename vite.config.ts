1de7055f5667a448da4f9a609c49a43c3979d7d3
Sávio Mendes <saviomd@users.noreply.github.com>
2026-07-09 22:35:47 -0300

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    ...(process.env.STORYBOOK ? [] : [reactRouter()]),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  plugins: [...(process.env.STORYBOOK ? [] : [reactRouter()]), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
